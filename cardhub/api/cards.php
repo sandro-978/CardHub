e1ad361b5591297e618775d696ff712f86a232a6
NotJenova <jenovafantays19@gmail.com>
2026-05-05 19:40:28 +0200

    Caricamento progetto

--- cardhub/api/cards.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-echo file_get_contents(__DIR__ . '/../data/sample-cards.json');
-?>
--- cardhub/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const alerts = document.querySelectorAll('[data-auto-hide]');
-
-    alerts.forEach(function (alert) {
-        setTimeout(function () {
-            alert.classList.add('d-none');
-        }, 3500);
-    });
-});
